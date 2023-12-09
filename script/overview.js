886741693949776eee1cc9af8bef8413658f1d2f
Ky <kyntt882@gmail.com>
2023-12-10 00:06:14 +0700

    Second attempt for bar chart, đang xu ly data cho pie voi line

--- script/overview.js
+++ script/overview.js
@@ -1,44 +1,59 @@
-let chartData = [];
+var chartData = [];
+var svg;
+
+const barChart = d3.select("#chartContainer");
+
+const chartWidth = 600;
+const chartHeight = 400;
+
+const margin = { top: 50, right: 20, bottom: 60, left: 100 };
+const width = chartWidth - margin.left - margin.right;
+const height = chartHeight - margin.top - margin.bottom;
+
+const tooltip = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
 
 function rowConvertor(row) {
     return {
       country: row['Country/Territory'],
-      population: +row['2022 Population']
+      population: +row['2022 Population'],
+      continent: row['Continent']
     };
   }
   
-d3.csv("../asset/data/world_population.csv", rowConvertor).then(data => {
+d3.csv(
+  "../asset/data/world_population.csv", 
+  rowConvertor
+  ).then(data => {
     // Sort the data by population in descending order
     data.sort((a, b) => b.population - a.population);
 
     // Filter only the top 20 countries by population
-    const top20 = data.slice(2,20);
+    const top20 = data.slice(0,20);
 
     // Create the bar chart
     createBarChart(top20);
-  }).catch(error => {
-    console.error("Error loading data:", error);
-  });
+    addButton(data);
+    deleteButton(data);
 
+  }).catch((error) => {
+    console.log(error);
+  });
 
 function createBarChart(data) {
     chartData = data;
 
-    const barChart = d3.select("#chartContainer");
-  
-    const chartWidth = 700;
-    const chartHeight = 500;
-
-    const margin = { top: 50, right: 20, bottom: 60, left: 100 };
-    const width = chartWidth - margin.left - margin.right;
-    const height = chartHeight - margin.top - margin.bottom;
-  
     // Define color scale
     const colorScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.population)])
         .range(["#cce599", "#004045"]);
 
-    const svg = barChart.append("svg")
+    const colorScale2 = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.population)])
+        .range(["#f7dfb5", "#d12421"]);
+
+    svg = barChart.append("svg")
       .attr("width", chartWidth)
       .attr("height", chartHeight)
       .append("g")
@@ -77,10 +92,6 @@ function createBarChart(data) {
       .style("fill", "white")
       .style("stroke", "white");
   
-    const tooltip = d3.select("body").append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0);
-
     // Draw vertical bars with colorScale
     svg.selectAll(".bar")
       .data(data)
@@ -92,6 +103,8 @@ function createBarChart(data) {
       .attr("height", d => height - yScale(d.population))
       .style("fill", d => colorScale(d.population))
       .on("mouseover", (event, d) => {
+        d3.select(event.currentTarget)
+          .style("fill", d => colorScale2(d.population));
         // Show tooltip on mouseover
         tooltip
           .style("opacity", 1)
@@ -99,7 +112,9 @@ function createBarChart(data) {
           .style("left", event.pageX + "px")
           .style("top", event.pageY + "px");
       })
-      .on("mouseout", () => {
+      .on("mouseout", (event, d) => {
+        d3.select(event.currentTarget)
+          .style("fill", d => colorScale(d.population));
         // Hide tooltip on mouseout
         tooltip.style("opacity", 0);
       });
@@ -109,62 +124,126 @@ function createBarChart(data) {
 
 function addButton() {
   const countryInput = document.getElementById("countryInput").value;
-  const countryData = chartData.find(d => d.country === countryInput);
+  const existingCountry = chartData.find(d => d.country.toLowerCase() === countryInput.toLowerCase());
 
-  if (countryData) {
-    // Add the country to the chart
-    chartData.push(countryData);
-    updateChart();
+  if (existingCountry) {
+    alert("Country already exists in the chart!");
+    return;
   } else {
-    console.log("Country not found.");
+    const newData = {
+      country: countryInput,
+      population: countryInput.population
+    };
+
+    chartData.push(newData);
+    updateChart(chartData);
   }
 }
 
 function deleteButton() {
   const countryInput = document.getElementById("countryInput").value;
-  const countryIndex = chartData.findIndex(d => d.country === countryInput);
+  const index = chartData.findIndex(d => d.country.toLowerCase() === countryInput.toLowerCase());
 
-  if (countryIndex !== -1) {
-    // Delete the country from the chart
-    chartData.splice(countryIndex, 1);
-    updateChart();
-  } else {
-    console.log("Country not found.");
+  if (index === -1) {
+    alert("Country does not exist in the chart!");
+    return;
   }
+
+  chartData.splice(index, 1);
+  updateChart(chartData);
 }
 
 function updateChart() {
-  const svg = d3.select("svg");
+  // Remove existing SVG chart
+  barChart.select("svg").remove();
+  
+  createBarChart(chartData);
+  
+  // const chartWidth = 700;
+  // const chartHeight = 500;
 
-  const xScale = d3.scaleBand()
-    .domain(chartData.map(d => d.country))
-    .range([0, width])
-    .padding(0.1);
+  // const margin = { top: 50, right: 20, bottom: 60, left: 100 };
+  // const width = chartWidth - margin.left - margin.right;
+  // const height = chartHeight - margin.top - margin.bottom;
+  
 
-  const yScale = d3.scaleLinear()
-    .domain([20, d3.max(chartData, d => d.population)])
-    .range([height,0]);
+  // // Select the SVG element
+  // svg = barChart.append("svg")
+  //   .attr("width", chartWidth)
+  //   .attr("height", chartHeight)
+  //   .append("g")
+  //   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  const colorScale = d3.scaleLinear()
-    .domain([0, d3.max(chartData, d => d.population)])
-    .range(["#cce599", "#004045"]);
 
-  svg.selectAll(".bar")
-    .data(chartData)
-    .join(
-      enter => enter.append("rect")
-        .attr("class", "bar")
-        .attr("x", d => xScale(d.country))
-        .attr("width", xScale.bandwidth())
-        .attr("y", d => yScale(d.population))
-        .attr("height", d => height - yScale(d.population))
-        .style("fill", d => colorScale(d.population)),
-      update => update
-        .attr("x", d => xScale(d.country))
-        .attr("width", xScale.bandwidth())
-        .attr("y", d => yScale(d.population))
-        .attr("height", d => height - yScale(d.population))
-        .style("fill", d => colorScale(d.population)),
-      exit => exit.remove()
-    );
+  // // Define color scale
+  // const colorScale = d3.scaleLinear()
+  //   .domain([0, d3.max(data, d => d.population)])
+  //   .range(["#cce599", "#004045"]);
+
+  // const updatedXScale = d3.scaleBand()
+  //   .domain(data.map(d => d.country))
+  //   .range([0, width])
+  //   .padding(0.1);
+
+  // const updatedYScale = d3.scaleLinear()
+  //   .domain([20, d3.max(data, d => d.population)])
+  //   .range([height, 0]);
+
+  // svg.select("g")
+  //   .attr("transform", `translate(0, ${height})`)
+  //   .call(d3.axisBottom(updatedXScale))
+  //   .style("fill", "white")
+  //   .selectAll("text")
+  //   .attr("transform", "rotate(-45)")
+  //   .style("text-anchor", "end")
+  //   .style("fill", "white");
+
+  // svg.select("g")
+  //   .call(d3.axisLeft(updatedYScale))
+  //   .style("fill", "white")
+  //   .style("stroke", "white");
+
+  // const bars = svg.selectAll(".bar")
+  //   .data(data, d => d.country);
+
+  // bars.exit()
+  //   .transition()
+  //   .duration(500)
+  //   .attr("height", 0)
+  //   .remove();
+
+  // const tooltip = d3.select("body").append("div")
+  //   .attr("class", "tooltip")
+  //   .style("opacity", 0);
+
+  // bars.enter().append("rect")
+  //   .attr("class", "bar")
+  //   .attr("x", d => updatedXScale(d.country))
+  //   .attr("width", updatedXScale.bandwidth())
+  //   .attr("y", height)
+  //   .attr("height", 0)
+  //   .merge(bars)
+  //   .transition()
+  //   .duration(500)
+  //   .attr("x", d => updatedXScale(d.country))
+  //   .attr("width", updatedXScale.bandwidth())
+  //   .attr("y", d => updatedYScale(d.population))
+  //   .attr("height", d => height - updatedYScale(d.population))
+  //   .style("fill", d => colorScale(d.population))
+  //   .on("mouseover", (event, d) => {
+  //     d3.select(event.currentTarget)
+  //       .style("fill", d => colorScale2(d.population));
+  //     // Show tooltip on mouseover
+  //     tooltip
+  //       .style("opacity", 1)
+  //       .html(`Country: ${d.country}<br>Population: ${d.population}`)
+  //       .style("left", event.pageX + "px")
+  //       .style("top", event.pageY + "px");
+  //   })
+  //   .on("mouseout", (event, d) => {
+  //     d3.select(event.currentTarget)
+  //       .style("fill", d => colorScale(d.population));
+  //     // Hide tooltip on mouseout
+  //     tooltip.style("opacity", 0);
+  //   });
 }
