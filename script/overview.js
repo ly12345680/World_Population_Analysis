c006be823bd56894ccbe871a7a47e6c80c1e7832
Ky <kyntt882@gmail.com>
2023-12-12 14:40:00 +0700

    Pie chart

--- script/overview.js
+++ script/overview.js
@@ -1,18 +1,18 @@
 var chartData = [];
 var svg;
 
-const barChart = d3.select("#chartContainer");
-
-const chartWidth = 600;
+const barChart = d3.select("#barChart");
+const chartWidth = 700;
 const chartHeight = 400;
 
 const margin = { top: 50, right: 20, bottom: 60, left: 100 };
 const width = chartWidth - margin.left - margin.right;
 const height = chartHeight - margin.top - margin.bottom;
-
 const tooltip = d3.select("body").append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
+
+
 
 function rowConvertor(row) {
     return {
@@ -32,16 +32,20 @@ d3.csv(
     // Filter only the top 20 countries by population
     const top20 = data.slice(0,20);
 
+    const percentageData = calculatePopulationPercentage(data);
+
     // Create the bar chart
     createBarChart(top20);
     addButton(data);
     deleteButton(data);
 
+    createPieChart(percentageData);
+
   }).catch((error) => {
     console.log(error);
   });
 
-// for the bar chart
+/* ************* for the bar chart part ************* */
 function createBarChart(data) {
     chartData = data;
 
@@ -63,11 +67,11 @@ function createBarChart(data) {
     // Add name of chart
     svg.append("text")
       .attr("class", "Chart-tittle")
-      .attr("x", width/2 - 70)
+      .attr("x", width/2 - 150)
       .attr("y", -30)
       .style("font-weight", "bold")
       .style("fill", "white")
-      .text("Top Countries by Population");
+      .text("Top Countries by Population in 2022");
 
 
     const xScale = d3.scaleBand()
@@ -139,7 +143,7 @@ function addButton() {
     chartData.push(newData);
     updateChart(chartData);
   }
-}
+} // end addButton
 
 function deleteButton() {
   const countryInput = document.getElementById("countryInput").value;
@@ -152,111 +156,165 @@ function deleteButton() {
 
   chartData.splice(index, 1);
   updateChart(chartData);
-}
+} // end deleteButton
 
 function updateChart() {
   // Remove existing SVG chart
   barChart.select("svg").remove();
   
+  chartData.sort((a, b) => b.population - a.population)
   createBarChart(chartData);
-  
-  // const chartWidth = 700;
-  // const chartHeight = 500;
+} // end function updateChart
+/* ************* end of bar chart part ************* */
 
-  // const margin = { top: 50, right: 20, bottom: 60, left: 100 };
-  // const width = chartWidth - margin.left - margin.right;
-  // const height = chartHeight - margin.top - margin.bottom;
-  
+/* ************* for the pie chart part ************* */
 
-  // // Select the SVG element
-  // svg = barChart.append("svg")
-  //   .attr("width", chartWidth)
-  //   .attr("height", chartHeight)
-  //   .append("g")
-  //   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+function calculatePopulationPercentage(data) {
+  const continentPopulation = d3.rollups(data, v => d3.sum(v, d => d.population), d => d.continent);
+  const totalPopulation = d3.sum(data, d => d.population);
 
+  const percentageData = continentPopulation.map(([continent, population]) => ({
+    continent: continent,
+    percentage: (population / totalPopulation) * 100
+  }));
 
-  // // Define color scale
-  // const colorScale = d3.scaleLinear()
-  //   .domain([0, d3.max(data, d => d.population)])
-  //   .range(["#cce599", "#004045"]);
-
-  // const updatedXScale = d3.scaleBand()
-  //   .domain(data.map(d => d.country))
-  //   .range([0, width])
-  //   .padding(0.1);
-
-  // const updatedYScale = d3.scaleLinear()
-  //   .domain([20, d3.max(data, d => d.population)])
-  //   .range([height, 0]);
-
-  // svg.select("g")
-  //   .attr("transform", `translate(0, ${height})`)
-  //   .call(d3.axisBottom(updatedXScale))
-  //   .style("fill", "white")
-  //   .selectAll("text")
-  //   .attr("transform", "rotate(-45)")
-  //   .style("text-anchor", "end")
-  //   .style("fill", "white");
-
-  // svg.select("g")
-  //   .call(d3.axisLeft(updatedYScale))
-  //   .style("fill", "white")
-  //   .style("stroke", "white");
-
-  // const bars = svg.selectAll(".bar")
-  //   .data(data, d => d.country);
-
-  // bars.exit()
-  //   .transition()
-  //   .duration(500)
-  //   .attr("height", 0)
-  //   .remove();
-
-  // const tooltip = d3.select("body").append("div")
-  //   .attr("class", "tooltip")
-  //   .style("opacity", 0);
-
-  // bars.enter().append("rect")
-  //   .attr("class", "bar")
-  //   .attr("x", d => updatedXScale(d.country))
-  //   .attr("width", updatedXScale.bandwidth())
-  //   .attr("y", height)
-  //   .attr("height", 0)
-  //   .merge(bars)
-  //   .transition()
-  //   .duration(500)
-  //   .attr("x", d => updatedXScale(d.country))
-  //   .attr("width", updatedXScale.bandwidth())
-  //   .attr("y", d => updatedYScale(d.population))
-  //   .attr("height", d => height - updatedYScale(d.population))
-  //   .style("fill", d => colorScale(d.population))
-  //   .on("mouseover", (event, d) => {
-  //     d3.select(event.currentTarget)
-  //       .style("fill", d => colorScale2(d.population));
-  //     // Show tooltip on mouseover
-  //     tooltip
-  //       .style("opacity", 1)
-  //       .html(`Country: ${d.country}<br>Population: ${d.population}`)
-  //       .style("left", event.pageX + "px")
-  //       .style("top", event.pageY + "px");
-  //   })
-  //   .on("mouseout", (event, d) => {
-  //     d3.select(event.currentTarget)
-  //       .style("fill", d => colorScale(d.population));
-  //     // Hide tooltip on mouseout
-  //     tooltip.style("opacity", 0);
-  //   });
+  return percentageData;
 }
-// end of bar chart part
 
-// for the pie chart
-var groupedData = d3.group(data, d => d["Continent"]);
+function createPieChart(data) {
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+  const radius = Math.min(width, height) / 2;
 
-var continentPopulations = [];
-groupedData.forEach(function(continentData, continent) {
-  var totalPopulation = continentData.reduce(function(sum, d) {
-    return sum + parseInt(d["2022 Population"]);
-  }, 0);
-  continentPopulations.push({ continent: continent, population: totalPopulation });
-});
+  const arc = d3.arc()
+    .innerRadius(60)
+    .outerRadius(radius - 2);
+
+  const pie = d3.pie()
+    .value(d => d.percentage);
+
+  const pieChart = d3.select("#pieChart")
+    .append("svg")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight)
+    .append("g")
+    .attr("transform", `translate(${chartWidth / 2}, ${chartHeight / 2})`);
+
+  pieChart.append("text")
+    .attr("class", "Chart-tittle")
+    .attr("x", - 150)
+    .attr("y", -radius - 30)
+    .style("font-weight", "bold")
+    .style("fill", "white")
+    .text("Population Percentage by Continent in 2022");
+
+  const arcs = pieChart.selectAll("arc")
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .attr("class", "arc")
+    .attr("stroke", "black")
+    //.attr("stroke-width", 1.5);
+
+  arcs.append("path")
+    .attr("d", arc)
+    .attr("fill", (d, i) => colorScale(i));
+
+  const legend = pieChart.selectAll(".legend")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => `translate(${radius + 20}, ${-radius + i * 20})`);
+  
+  legend.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 15)
+    .attr("height", 15)
+    .attr("fill", (d, i) => colorScale(i));
+
+  legend.append("text")
+    .attr("x", 20)
+    .attr("y", 12)
+    .style("font-size", "14px")
+    .style("fill", "white")
+    .text(d => `${d.continent}: ${d.percentage.toFixed(2)}%`);
+}
+// var groupedData = d3.group(data, d => d["Continent"]);
+// var totalPopulation = d3.sum(data, d => parseInt(d["2022 Population"]));
+
+// var continentPopulations = [];
+// groupedData.forEach(function(continentData, continent) {
+//   //var population = d3.sum(continentData, d => parseInt(d['2022 Popultion']));
+//   var percentage = (population / totalPopulation) * 100;
+//   continentPopulations.push({ continent: continent, percentage: percentage });
+// });
+
+
+
+// function createPieChart(){
+//   const pieChart = d3.select("#chartContainer");
+
+//   const pieWidth = 400;
+//   const pieHeight = 300;
+//   const pieRadius = Math.min(pieWidth, pieHeight) / 2;
+
+//   const pieSvg = pieChart.append("svg")
+//   .attr("width", pieWidth)
+//   .attr("height", pieHeight)
+//   .append("g")
+//   .attr("transform", "translate(" + pieWidth / 2 + "," + pieHeight / 2 + ")")
+//   .attr("stroke", "black")
+//   .attr("stroke-width", 1);
+
+//   const pieColor = d3.scaleOrdinal()
+//     .domain(continentPopulations.map(function(d) { return d.continent; }))
+//     .range(d3.schemeCategory10);
+
+//   const pie = d3.pie()
+//     .value(function(d) { return d.percentage; });
+
+//   const piePath = d3.arc()
+//     .outerRadius(pieRadius - 2)
+//     .innerRadius(60);
+
+//   const pieArc = pieSvg.selectAll("arc")
+//     .data(pie(continentPopulations))
+//     .enter().append("g")
+//     .attr("class", "arc");
+
+//   pieArc.append("path")
+//     .attr("d", piePath)
+//     .attr("fill", function(d) { return pieColor(d.data.continent); });
+
+//   // Add labels to the pie chart
+//   // const pieLabels = pieSvg.selectAll("text")
+//   //   .data(pie(continentPopulations))
+//   //   .enter().append("text")
+//   //   .attr("transform", function(d) { return "translate(" + piePath.centroid(d) + ")"; })
+//   //   .attr("dy", "0.35em")
+//   //   .text(function(d) { return d.data.continent + " (" + d.data.percentage.toFixed(2) + "%)"; })
+//   //   .attr("text-anchor", "middle")
+//   //   .attr("fill", "white");
+
+//   // Add legend to the pie chart
+//   const legend = pieSvg.selectAll(".legend")
+//     .data(pie(continentPopulations))
+//     .enter().append("g")
+//     .attr("class", "legend")
+//     .attr("transform", (d, i) => "translate(180," + (i * 25 - 80) + ")");
+
+//   legend.append("rect")
+//     .attr("x", pieWidth - 18)
+//     .attr("width", 18)
+//     .attr("height", 18)
+//     .attr("fill", (d, i) => pieColor(i));
+
+//   legend.append("text")
+//     .attr("x", pieWidth - 24)
+//     .attr("y", 9)
+//     .attr("dy", "0.35em")
+//     .style("text-anchor", "end")
+//     .text((d, i) => `${continentPopulations[i].continent} (${continentPopulations[i].percentage.toFixed(2)}%)`);
+// }
+/* ************* end of pie chart part ************* */
